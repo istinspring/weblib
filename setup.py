29dc2946b1a4e0588aee29a8522658080d307995
Gregory Petukhov <lorien@lorien.name>
2015-09-25 00:26:15 +0600

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = 'weblib',
-    version = '0.1.15',
+    version = '0.1.16',
     description = 'Set of tools for web scraping projects',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
